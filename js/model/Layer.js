74928079846c744ff5fa2cb000749c32c0fefc31
Moi C Noé <55990@etu.he2b.be>
2021-04-09 22:22:22 +0200

    étape 1.6 : layer

---
+++ js/model/Layer.js
@@ -0,0 +1,52 @@
+/**
+ * Layer is a screen over the board with the same type of objects.
+ */
+class Layer {
+
+    /**
+     * 
+     * @param {number} nbRows 
+     * @param {number} nbColumns 
+     */
+    constructor(nbRows, nbColumns) {
+        this._array = Array(nbRows).fill().map(() => Array(nbColumns));
+    }
+
+    /**
+     * 
+     * @param {Position} pos 
+     * @returns {boolean}
+     */
+    contains(pos) {
+        return 0 <= pos.row && pos.row < this._array.length && 0 <= pos.column && pos.column < this._array[0].length;
+    }
+
+    /**
+     * @param {Position} pos
+     * @param {Tile} tile
+     */
+    setTile(pos, tile) {
+        if (!this.contains(pos)) throw "Position is not inside the board.";
+        this._array[pos.row][pos.column] = tile;
+    }
+
+    /**
+     * 
+     * @param {Position} pos 
+     * @returns {Tile}
+     */
+    getTile(pos) {
+        if (!this.contains(pos)) throw "Position is not inside the board.";
+        return this._array[pos.row][pos.column];
+    }
+
+    /**
+     * 
+     * @param {Position} pos 
+     * @returns {boolean}
+     */
+    hasTile(pos) {
+        if (!this.contains(pos)) throw "Position is not inside the board.";
+        return typeof this._array[pos.row][pos.column] != 'undefined';
+    }
+}
